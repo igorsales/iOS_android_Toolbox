712b7fe5e99b0d5ed85a409d48e4e7c1305e4ae0
Igor Sales <self@igorsales.ca>
2011-12-23 21:35:36 -0500

    Added auto-search to get_version.rb

--- bin/get_version.rb
+++ bin/get_version.rb
@@ -8,6 +8,12 @@ require 'ios_android_toolbox/android'
 include IosAndroidToolbox
 
 version_file = ARGV.shift
+if version_file.nil?
+    files = IosVersionController.find_project_info << AndroidVersionController.find_project_info
+    files.flatten!
+    
+    version_file = files.first if files.length == 1
+end
 raise "Please specify the version file" if version_file.nil?
 
 ctrl = nil
