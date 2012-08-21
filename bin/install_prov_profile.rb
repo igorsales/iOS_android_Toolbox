b7b42c2219cd8dc72c3eff8ac937bd38ab368592
Igor Sales <self@igorsales.ca>
2012-08-21 15:16:14 -0400

    v0.0.14
    * Modularized installing prov profiles

--- bin/install_prov_profile.rb
+++ bin/install_prov_profile.rb
@@ -3,83 +3,17 @@
 require 'rubygems'
 require 'parsedate'
 require 'time'
+require 'ios_android_toolbox/ios_prov_profile'
 
-DEBUG=false
-PROV_PROFILE_DIR=ENV['HOME']+'/Library/MobileDevice/Provisioning Profiles'
-UUID_REGEX='[0-9A-Za-z]{8}-[0-9A-Za-z]{4}-[0-9A-Za-z]{4}-[0-9A-Za-z]{4}-[0-9A-Za-z]{12}'
+include IosAndroidToolbox
 
-profile_file = ARGV.shift
-profile_file or raise "Please specify a provisioning profile name"
+class InstallProvProfile
+    def run(args)
+        profile_path = args.shift
+        profile_path or raise "Please specify a provisioning profile name"
 
-prov_profile = File.open(profile_file).read
-
-def uuid_for_profile(contents)
-  # <key>UUID</key>
-  #  <string>06AF2826-608D-4CE9-99AE-AA917FF1641E</string>
-  if /<key>UUID<\/key>\s*<string>(#{UUID_REGEX})<\/string>/.match(contents)
-    puts "Found UUID: #{$1}" if DEBUG
-    uuid = $1
-  else
-    nil
-  end
+        IosProvisioningProfile.install(profile_path)
+    end
 end
 
-def app_id_from_profile(contents)
-  # <key>application-identifier</key>
-  # <string>NDVAA33T9J.com.favequest.FFSApp.87.ircpa</string>
-  if /<key>application-identifier<\/key>\s*<string>[A-Za-z0-9]+\.([^<]+)<\/string>/.match(contents)
-    puts "Found app Id: #{$1}" if DEBUG
-    app_id = $1
-  else
-    nil
-  end
-end
-
-def creation_date_from_profile(contents)
-  # <key>CreationDate</key>
-  # <date>2011-08-30T02:11:55Z</date>
-  if /<key>CreationDate<\/key>\s*<date>([^<]+)<\/date>/.match(contents)
-    #creation_date = Date.strptime($1, '%Y-%m-%dT%h:%M:%sZ')
-    creation_date = Time.parse($1)
-    puts "Found Creation date: #{creation_date.to_s}" if DEBUG
-    creation_date
-  else
-    nil
-  end
-end
-
-def has_provisioned_devices(contents)
-  # <key>ProvisionedDevices</key>
-  /<key>ProvisionedDevices<\/key>/.match(contents)
-end
-
-new_uuid   = uuid_for_profile(prov_profile)
-new_app_id = app_id_from_profile(prov_profile)
-new_cdate  = creation_date_from_profile(prov_profile)
-new_is_dev = has_provisioned_devices(prov_profile)
-
-`mkdir -p "(#{PROV_PROFILE_DIR}"; chmod 755 "#{PROV_PROFILE_DIR}"` if !File.directory?(PROV_PROFILE_DIR)
-
-# Look through each file in the list
-Dir.foreach(PROV_PROFILE_DIR) do |item|
-  next if item == '.' or item == '..'
-
-  next if !/#{UUID_REGEX}\.mobileprovision/.match(item)
-  puts "Prov. profile: #{item}" if DEBUG
-
-  old_prov_profile = File.open(PROV_PROFILE_DIR+"/"+item).read
-
-  old_uuid   = uuid_for_profile(old_prov_profile)
-  old_app_id = app_id_from_profile(old_prov_profile)
-  old_cdate  = creation_date_from_profile(old_prov_profile)
-  old_is_dev = has_provisioned_devices(old_prov_profile)
-
-  if old_app_id == new_app_id and old_cdate < new_cdate and old_is_dev == new_is_dev
-    puts "Removing stale Prov Profile: #{item}"
-    File.unlink PROV_PROFILE_DIR+'/'+item
-  end
-end
-
-new_profile_path = PROV_PROFILE_DIR+"/#{new_uuid}.mobileprovision"
-puts "Installing new prov profile into #{new_profile_path}"
-`cp -f "#{profile_file}" "#{new_profile_path}"`
+InstallProvProfile.new.run(ARGV)
---
+++ lib/ios_android_toolbox/ios_prov_profile.rb
@@ -0,0 +1,128 @@
+require 'fileutils'
+
+module IosAndroidToolbox
+	class IosProvisioningProfile
+		PROV_PROFILE_DIR=ENV['HOME']+'/Library/MobileDevice/Provisioning Profiles'
+		UUID_REGEX='[0-9A-Za-z]{8}-[0-9A-Za-z]{4}-[0-9A-Za-z]{4}-[0-9A-Za-z]{4}-[0-9A-Za-z]{12}'
+
+		DEBUG=false
+
+		attr_reader :contents
+
+		def uuid
+		  # <key>UUID</key>
+		  #  <string>06AF2826-608D-4CE9-99AE-AA917FF1641E</string>
+		  if /<key>UUID<\/key>\s*<string>(#{UUID_REGEX})<\/string>/.match(contents)
+		    puts "Found UUID: #{$1}" if DEBUG
+		    uuid = $1
+		  else
+		    nil
+		  end
+		end
+
+		def app_id
+		  # <key>application-identifier</key>
+		  # <string>NDVAA33T9J.com.favequest.FFSApp.87.ircpa</string>
+		  if /<key>application-identifier<\/key>\s*<string>[A-Za-z0-9]+\.([^<]+)<\/string>/.match(contents)
+		    puts "Found app Id: #{$1}" if DEBUG
+		    app_id = $1
+		  else
+		    nil
+		  end
+		end
+
+		def creation_date
+		  # <key>CreationDate</key>
+		  # <date>2011-08-30T02:11:55Z</date>
+		  if /<key>CreationDate<\/key>\s*<date>([^<]+)<\/date>/.match(contents)
+		    #creation_date = Date.strptime($1, '%Y-%m-%dT%h:%M:%sZ')
+		    creation_date = Time.parse($1)
+		    puts "Found Creation date: #{creation_date.to_s}" if DEBUG
+		    creation_date
+		  else
+		    nil
+		  end
+		end
+
+		def has_provisioned_devices?
+		  # <key>ProvisionedDevices</key>
+		  /<key>ProvisionedDevices<\/key>/.match(contents)
+		end
+
+		def initialize(contents)
+			# If we specify a path, read it first
+			begin
+				if File.exists? contents and not File.directory? contents
+					contents = File.open(contents).read
+				end
+			rescue
+			end
+			@contents = contents
+		end
+
+		def self.remove_stale_equivalent_profiles(path)
+			new_profile = IosProvisioningProfile.new(path)
+
+			# Look through each file in the list
+			# Dir.glob(File.join(PROV_PROFILE_DIR,"*.mobileprovision")) do |installed_profile_path|
+  
+			# 	next if not /#{UUID_REGEX}\.mobileprovision$/.match(installed_profile_path)
+  	# 			puts "Examining prov. profile: #{installed_profile_path}" if DEBUG
+
+  	# 			installed_profile = IosProvisioningProfile.new(File.open(installed_profile_path).read)
+
+  			self.loop_through_existing_profiles do |installed_profile|
+  				if installed_profile.app_id == new_profile.app_id and 
+	 			   installed_profile.creation_date < new_profile.creation_date and 
+	 			   installed_profile.has_provisioned_devices? == new_profile.has_provisioned_devices?
+					puts "Removing stale Prov Profile: #{installed_profile_path}"
+    				File.delete installed_profile_path
+  				end
+			end
+		end
+
+		def self.install(path)
+			if not File.directory? PROV_PROFILE_DIR
+				FileUtils.mkdir_p PROV_PROFILE_DIR
+				FileUtils.chmod 0755, PROV_PROFILE_DIR
+			end
+
+			new_profile = IosProvisioningProfile.new(path)
+			new_path    = File.join(PROV_PROFILE_DIR, new_profile.uuid+".mobileprovision")
+
+			if File.expand_path(path) != File.expand_path(new_path)
+				if profile_worth_installing? path
+					self.remove_stale_equivalent_profiles(path)
+					FileUtils.copy(path, new_path)
+				end
+			else
+				puts "Cannot install new profile over itself!"
+			end
+		end
+
+		def self.profile_worth_installing?(path)
+			new_profile = IosProvisioningProfile.new(path)
+			loop_through_existing_profiles do |installed_profile|
+  				if installed_profile.app_id == new_profile.app_id
+	 			  	return false if installed_profile.creation_date >= new_profile.creation_date 
+  				end
+			end
+
+			true
+		end
+
+		private
+		def self.loop_through_existing_profiles(&block)
+			# Look through each file in the list
+			Dir.glob(File.join(PROV_PROFILE_DIR,"*.mobileprovision")) do |installed_profile_path|
+  
+				next if not /#{UUID_REGEX}\.mobileprovision$/.match(installed_profile_path)
+  				puts "Examining prov. profile: #{installed_profile_path}" if DEBUG
+
+  				installed_profile = IosProvisioningProfile.new(File.open(installed_profile_path).read)
+
+  				yield installed_profile
+			end
+		end
+	end
+end
